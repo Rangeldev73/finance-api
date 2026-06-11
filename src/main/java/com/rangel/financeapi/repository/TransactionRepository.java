4db5c067cac23a26c63f8e3019eca413659f8c48
Rangel <joaquimrangel.dev@gmail.com>
2026-06-11 20:16:24 -0300

    feat: add UserRepository and TransactionRepository

---
+++ src/main/java/com/rangel/financeapi/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.rangel.financeapi.repository;
+
+import com.rangel.financeapi.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface TransactionRepository  extends JpaRepository<Transaction,  Long> {
+    List<Transaction> findByUserId(Long id);
+}
